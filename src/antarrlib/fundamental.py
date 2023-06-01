70ab0bca86702f71742e7fb9ad996e36c0956836
Taishi Hashimoto <hashimoto.taishi@outlook.com>
2023-06-01 11:27:05 +0900

    refactor: decibel functions are moved into individual module

--- src/antarrlib/fundamental.py
+++ src/antarrlib/fundamental.py
@@ -76,38 +76,3 @@ def radial(ze: np.ndarray, az: np.ndarray) -> np.ndarray:
         np.sin(ze) * np.cos(az),
         np.sin(ze) * np.sin(az),
         np.cos(ze)))
-
-
-def dB(x, b=1) -> np.ndarray:
-    """Decibel function, i.e., `y := 10*log10(x/b)`.
-
-    Parameters
-    ==========
-    x: array-like
-        Input value.
-    b: array-like
-        Base for decibel. Default is 1.
-
-    Returns
-    =======
-    array-like with the same shape with `x`.
-        Decibel value.
-    """
-    if isinstance(b, (int, float, str)):
-        if b == "max":
-            b = np.nanmax(x)
-        elif b == "min":
-            b = np.nanmin(x)
-        elif b == 0:
-            b = np.full_like(x, np.nan)
-    else:
-        b = np.array(b, dtype=float)
-        b[b == 0] = np.nan
-    x = np.array(x, dtype=float)
-    x[x <= 0] = np.nan
-    return 10 * np.log10(x / b)
-
-
-def idB(y) -> np.ndarray:
-    """Inverse function of `dB`, i.e., `x := 10^(y/10)`."""
-    return np.power(10, 0.1*np.array(y))
