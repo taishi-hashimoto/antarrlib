616e7c2a37048bc6d031ca560a3fd54c4c24d349
Taishi Hashimoto <taishi.hashimoto@eiscat.se>
2025-05-04 14:05:12 +0900

    fix: steering_vector is now in numpy

--- src/antarrlib/imaging.py
+++ src/antarrlib/imaging.py
@@ -1,17 +1,16 @@
+"Imaging library."
 import numpy as np
-import jax
-import jax.numpy as jnp
 
 
-def subrange(r0: np.ndarray, rr: np.ndarray, nsubr: int) -> np.ndarray:
-    """Compute subrange gates for each range gate.
+def subrange_centers(r0: np.ndarray, rr: np.ndarray, nsubr: int) -> np.ndarray:
+    """Compute the center of the subrange gates for each range gate bin.
 
     Assuming that the radar is located at the origin.
 
     Parameters
     ==========
     r0: ndarray
-        Lower bounds the range gate [nhigh]
+        Lower bounds of the original range gate grid [nhigh]
     rr: ndarray
         The range resolution
     nsubr: int
@@ -20,19 +19,18 @@ def subrange(r0: np.ndarray, rr: np.ndarray, nsubr: int) -> np.ndarray:
     Returns
     =======
     c: float
-        Subranges [nhigh, nsubr]
+        Subrange centers [nhigh, nsubr]
     """
     csubr = (np.arange(nsubr) - nsubr/2 + 0.5) / nsubr + 0.5
     return np.reshape(r0, (-1, 1)) + rr * np.reshape(csubr, (1, -1))
 
 
 def steering_vector(
-    k: jnp.ndarray,
-    r: jnp.ndarray,
-    v: jnp.ndarray,
-    c: jnp.ndarray,
-    s: int = 1
-) -> jnp.ndarray:
+    k: np.ndarray,
+    p: np.ndarray,
+    v: np.ndarray,
+    r: np.ndarray,
+) -> np.ndarray:
     """Generalized steering vector for frequency and spatial domain interferometry.
 
     The result is not normalized.
@@ -40,39 +38,36 @@ def steering_vector(
     Parameters
     ==========
     k: ndarray
-        The wave number [nfreq].
-    r: ndarray
-        The antenna location [nant, 3].
+        The wave number of the size `[nfreq]`, where `nfreq` is the number of frequencies.
+    p: ndarray
+        The antenna location of the size `[nant, 3]`, where `nant` is the number of antennas.
     v: ndarray
-        Radial vectors [ndir, 3], where ndir is the number of directions.
-    c: ndarray
-        The range and subrange gate bounds [nr, nsubr], wherenr is the number of range gate and
-        nsubr is the number of subdivision. Can be computed by subrange().
-    s: int
-        +1 for Tx, -1 for Rx.
+        Radial vectors of the size `[ndir, 3]`, where `ndir` is the number of directions.
+    r: ndarray
+        The range and subrange gate bounds `[nr, nsubr]`, where `nr` is the number of range gate and
+        `nsubr` is the number of subdivision. Can be computed by `subrange()`.
 
     Returns
     =======
     a: ndarray
-        Steering vector [nr, ndir, nsubr, nfreq, nant]
+        Steering vector of the size `[nr, ndir, nsubr, nfreq, nant]`.
     """
-    v = jnp.reshape(v, (-1, 3))
-    c = jnp.atleast_2d(c)
-
-    ndir = len(v)
-    nr, nsubr = c.shape
+    k = np.ravel(k)
+    p = np.reshape(p, (-1, 3))
+    v = np.reshape(v, (-1, 3))
+    r = np.atleast_2d(r)[:, None, :, None]
 
     # Tx distance is the subrange gate bounds.
     # Here, we assume that c is measured from the center of the antenna array.
-    tx_distance = c[:, None, :, None]
+    tx_distance = r
 
     # Rx positions are measured from the center of the antenna array.
-    rx_positions = v[None, :, None, :] * c[:, None, :, None] + jnp.mean(r, axis=0)[None, None, None, :]
+    rx_positions = v[None, :, None, :] * r + np.mean(p, axis=0)[None, None, None, :]
 
     # Rx distance is measured for each antenna.
-    rx_distance = jnp.linalg.norm(rx_positions[..., None, :] - r[None, ..., :], axis=-1)
-    
+    rx_distance = np.linalg.norm(rx_positions[..., None, :] - p[None, ..., :], axis=-1)
+
     distance = tx_distance + rx_distance
-    a = jnp.exp(1j * k[None, :, None] * distance[..., None, :])
+    a = np.exp(1j * k[None, :, None] * distance[..., None, :])
 
     return a
